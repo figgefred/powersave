0738d540724c630495e65ee389c621892b719c36
root <root@Crunchy.(none)>
2014-02-10 23:10:52 +0100

    Some changes and customizations

--- scripts/powersave.sh
+++ scripts/powersave.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
 case $1/$2 in
   pre/*)
-	/usr/bin/powersave false ;;
+	/usr/bin/powersave false ;
+	echo 0 >/usr/local/etc/powersave/active ;;
   post/*)
 	if cat /sys/class/power_supply/ADP1/online | grep 0 > /dev/null 2>&1
 	then
