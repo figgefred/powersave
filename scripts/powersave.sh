4cf2cd9fd21d8b5eef7070d1bc9a178e29b167a5
root <root@Crunchy.(none)>
2014-02-14 21:38:53 +0100

    Some corrections. And powersave_manual feature adds.

--- scripts/powersave.sh
+++ scripts/powersave.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 case $1/$2 in
   pre/*)
-	/usr/bin/powersave false ;
-	echo 0 >/usr/local/etc/powersave/active ;;
+	/usr/bin/powersave false ;;
   post/*)
-	if cat /sys/class/power_supply/ADP1/online | grep 0 > /dev/null 2>&1
-	then
-		/usr/bin/powersave true
-	else
-		/usr/bin/powersave false
-	fi
+	/usr/bin/powersave
 	;;
 esac
